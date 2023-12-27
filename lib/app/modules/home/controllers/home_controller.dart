5eab566aca4c1c77eafb43d7e557d3406f83767e
Farhan <nefarha1@gmail.com>
2023-12-27 14:17:09 +0700

    Added View for Product

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:simple_ecommerce/app/data/model/product_model.dart';
+import 'package:simple_ecommerce/app/services/api_service.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
+class HomeController extends GetxController with StateMixin<List<Product>> {
+  final apiService = ApiServices();
+  final scrollController = ScrollController();
 
-  final count = 0.obs;
+  RxList<Product> daftarProduk = RxList.empty();
+
   @override
   void onInit() {
+    apiService.readDataLimit().then((value) {
+      daftarProduk.value = value;
+      change(value, status: RxStatus.success());
+    });
+
     super.onInit();
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
+import 'package:simple_ecommerce/app/data/reusable/reusable_item.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -13,11 +14,28 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: controller.obx(
+        (state) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+          child: buildProductView(),
         ),
+      ),
+    );
+  }
+
+  Widget buildProductView() {
+    return Obx(
+      () => MasonryGridView.builder(
+        controller: controller.scrollController,
+        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2),
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 3,
+        itemCount: controller.daftarProduk.length,
+        itemBuilder: (context, index) {
+          var model = controller.daftarProduk[index];
+          return cardProduct(model: model);
+        },
       ),
     );
   }
