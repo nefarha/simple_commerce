9e4c257eeb95d7283043327504bbc367f7b04104
Farhan <nefarha1@gmail.com>
2023-12-27 13:24:42 +0700

    Added Product Model

---
+++ lib/app/data/model/product_model.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class Product {
+  int id;
+  String title;
+  String description;
+  int price;
+  double discountPercentage;
+  double rating;
+  int stock;
+  String brand;
+  String category;
+  String thumbnail;
+  List<String> images;
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
+    required this.stock,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  Product copyWith({
+    int? id,
+    String? title,
+    String? description,
+    int? price,
+    double? discountPercentage,
+    double? rating,
+    int? stock,
+    String? brand,
+    String? category,
+    String? thumbnail,
+    List<String>? images,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      discountPercentage: discountPercentage ?? this.discountPercentage,
+      rating: rating ?? this.rating,
+      stock: stock ?? this.stock,
+      brand: brand ?? this.brand,
+      category: category ?? this.category,
+      thumbnail: thumbnail ?? this.thumbnail,
+      images: images ?? this.images,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'price': price,
+      'discountPercentage': discountPercentage,
+      'rating': rating,
+      'stock': stock,
+      'brand': brand,
+      'category': category,
+      'thumbnail': thumbnail,
+      'images': images,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      price: map['price'] as int,
+      discountPercentage: map['discountPercentage'] as double,
+      rating: map['rating'] as double,
+      stock: map['stock'] as int,
+      brand: map['brand'] as String,
+      category: map['category'] as String,
+      thumbnail: map['thumbnail'] as String,
+      images: List<String>.from((map['images'] as List<String>)),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Product(id: $id, title: $title, description: $description, price: $price, discountPercentage: $discountPercentage, rating: $rating, stock: $stock, brand: $brand, category: $category, thumbnail: $thumbnail, images: $images)';
+  }
+
+  @override
+  bool operator ==(covariant Product other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.title == title &&
+        other.description == description &&
+        other.price == price &&
+        other.discountPercentage == discountPercentage &&
+        other.rating == rating &&
+        other.stock == stock &&
+        other.brand == brand &&
+        other.category == category &&
+        other.thumbnail == thumbnail &&
+        listEquals(other.images, images);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        title.hashCode ^
+        description.hashCode ^
+        price.hashCode ^
+        discountPercentage.hashCode ^
+        rating.hashCode ^
+        stock.hashCode ^
+        brand.hashCode ^
+        category.hashCode ^
+        thumbnail.hashCode ^
+        images.hashCode;
+  }
+}
